ad4b06cc7a6e57e571da98eeddb43cfc7721e19c
David Miranda <david@storylog.com>
2018-04-08 18:44:00 -0400

    email input error showing up

--- src/js/_emailChecker.js
+++ src/js/_emailChecker.js
@@ -2,6 +2,7 @@
   const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const placeholderElement = $(".preview__info-email-placeholder");
   const inputElement = $(".preview__info-email-input");
+  const emailErrorElement = $(".preview__email-error");
 
   const hidePlaceholder = function () {
     setVisibility(false, placeholderElement);
@@ -15,9 +16,9 @@
 
   const checkEmailValid = function () {
     if (inputElement.value.length === 0 || emailRegex.test(inputElement.value)) {
-      console.log("email valid!");
+      emailErrorElement.classList.add("visibility-hidden");
     } else {
-      console.log("email INVALID!");
+      emailErrorElement.classList.remove("visibility-hidden");
     }
   }
 
