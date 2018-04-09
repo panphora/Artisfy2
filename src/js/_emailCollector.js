7f10f50ac2016d80fd19b6776e4731ea146d30fb
David Miranda <david@storylog.com>
2018-04-08 23:19:06 -0400

    new monitor icon, added focused style

--- src/js/_emailCollector.js
+++ src/js/_emailCollector.js
@@ -2,6 +2,7 @@
   const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const placeholderElement = $(".preview__info-email-placeholder");
   const inputElement = $(".preview__info-email-input");
+  const inputBgElement = $(".preview__info-email");
   const emailErrorElement = $(".preview__email-error");
   const emailForm = $(".preview__info-email-section");
 
@@ -21,11 +22,21 @@
     } else {
       emailErrorElement.classList.remove("visibility-hidden");
     }
-  }
+  };
+
+  const addFocusedSelector = function () {
+    inputBgElement.classList.add("preview__info-email--focused");
+  };
+
+  const removeFocusedSelector = function () {
+    inputBgElement.classList.remove("preview__info-email--focused");
+  };
 
   inputElement.on("blur", showPlaceholder);
   inputElement.on("blur", checkEmailValid);
+  inputElement.on("blur", removeFocusedSelector);
   inputElement.on("focus", hidePlaceholder);
+  inputElement.on("focus", addFocusedSelector);
   placeholderElement.on("click", hidePlaceholder);
 
   emailForm.on("submit", function (event) {
