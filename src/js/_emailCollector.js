8a0c08cef130895c36adbd1fde39ca33e8e29fcf
David Miranda <david@storylog.com>
2018-04-10 22:20:38 -0400

    support for multiple email collections

--- src/js/_emailCollector.js
+++ src/js/_emailCollector.js
@@ -1,52 +1,57 @@
 (function () {
   const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  const placeholderElement = $(".preview__info-email-placeholder");
-  const inputElement = $(".preview__info-email-input");
-  const inputBgElement = $(".preview__info-email");
-  const emailErrorElement = $(".preview__email-error");
-  const emailForm = $(".preview__info-email-section");
 
-  const hidePlaceholder = function () {
-    setVisibility(false, placeholderElement);
-  };
+  const emailCollectElements = $$(".email-collect");
 
-  const showPlaceholder = function () {
-    if (inputElement.value.trim().length < 1) {
-      setVisibility(true, placeholderElement);
-    }
-  };
+  emailCollectElements.forEach(function (emailCollectElement) {
+    const placeholderElement = emailCollectElement.querySelector(".email-collect__placeholder");
+    const inputElement = emailCollectElement.querySelector(".email-collect__input");
+    const inputBgElement = emailCollectElement.querySelector(".email-collect__email-container");
+    const emailErrorElement = emailCollectElement.querySelector(".email-collect__error");
+    const emailForm = emailCollectElement.querySelector(".email-collect__form");
 
-  const checkEmailValid = function () {
-    if (inputElement.value.trim().length === 0 || emailRegex.test(inputElement.value.trim())) {
-      emailErrorElement.classList.add("visibility-hidden");
-    } else {
-      emailErrorElement.classList.remove("visibility-hidden");
-    }
-  };
+    const hidePlaceholder = function () {
+      setVisibility(false, placeholderElement);
+    };
 
-  const addFocusedSelector = function () {
-    inputBgElement.classList.add("preview__info-email--focused");
-  };
+    const showPlaceholder = function () {
+      if (inputElement.value.trim().length < 1) {
+        setVisibility(true, placeholderElement);
+      }
+    };
 
-  const removeFocusedSelector = function () {
-    inputBgElement.classList.remove("preview__info-email--focused");
-  };
+    const checkEmailValid = function () {
+      if (inputElement.value.trim().length === 0 || emailRegex.test(inputElement.value.trim())) {
+        emailErrorElement.classList.add("visibility-hidden");
+      } else {
+        emailErrorElement.classList.remove("visibility-hidden");
+      }
+    };
 
-  inputElement.on("blur", showPlaceholder);
-  inputElement.on("blur", checkEmailValid);
-  inputElement.on("blur", removeFocusedSelector);
-  inputElement.on("focus", hidePlaceholder);
-  inputElement.on("focus", addFocusedSelector);
-  placeholderElement.on("click", hidePlaceholder);
+    const addFocusedSelector = function () {
+      inputBgElement.classList.add("email-collect__email-container--focused");
+    };
 
-  emailForm.on("submit", function (event) {
-    if (inputElement.value.trim().length === 0) {
-      event.preventDefault();
-      inputElement.focus();
-    } else if (!emailRegex.test(inputElement.value.trim())) {
-      event.preventDefault();
-      checkEmailValid();
-      inputElement.focus();
-    }
+    const removeFocusedSelector = function () {
+      inputBgElement.classList.remove("email-collect__email-container--focused");
+    };
+
+    inputElement.on("blur", showPlaceholder);
+    inputElement.on("blur", checkEmailValid);
+    inputElement.on("blur", removeFocusedSelector);
+    inputElement.on("focus", hidePlaceholder);
+    inputElement.on("focus", addFocusedSelector);
+    placeholderElement.on("click", hidePlaceholder);
+
+    emailForm.on("submit", function (event) {
+      if (inputElement.value.trim().length === 0) {
+        event.preventDefault();
+        inputElement.focus();
+      } else if (!emailRegex.test(inputElement.value.trim())) {
+        event.preventDefault();
+        checkEmailValid();
+        inputElement.focus();
+      }
+    });
   });
 })();
